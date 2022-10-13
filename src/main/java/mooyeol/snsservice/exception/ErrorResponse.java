c6db36db92521a371f7acaa9aee71853b6f1ff67
Lee-moo <dlandif22@gmail.com>
2022-10-13 16:57:14 +0900

    add ErrorResponse

---
+++ src/main/java/mooyeol/snsservice/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package mooyeol.snsservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private final String message;
+}
