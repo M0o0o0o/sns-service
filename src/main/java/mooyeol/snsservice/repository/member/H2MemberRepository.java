06085ec4536f96b8c157283014e4468a7d4eeb8c
Lee-moo <dlandif22@gmail.com>
2022-10-18 08:09:25 +0900

    Change Member(controller, service repository)
    - Add logout API

--- src/main/java/mooyeol/snsservice/repository/member/H2MemberRepository.java
+++ src/main/java/mooyeol/snsservice/repository/member/H2MemberRepository.java
@@ -6,6 +6,8 @@ import mooyeol.snsservice.domain.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -22,7 +24,7 @@ public class H2MemberRepository implements MemberRepository {
     @Override
     public Optional<Member> findByEmail(String email) {
         try {
-            String query = "select m from Member m where m.email = :email";
+            String query = "select m from Member m where m.email=:email";
             Member member = em.createQuery(query, Member.class).setParameter("email", email).getSingleResult();
             return Optional.of(member);
         } catch (NoResultException e) {
