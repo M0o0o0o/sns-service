f27d915496fce8f9a65d4fa48fbfb364dcdf9ea7
Lee-moo <dlandif22@gmail.com>
2022-10-25 20:21:12 +0900

    Add PostTagRepository

---
+++ src/main/java/mooyeol/snsservice/repository/PostTagRepository.java
@@ -0,0 +1,8 @@
+package mooyeol.snsservice.repository;
+
+import mooyeol.snsservice.domain.PostTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostTagRepository extends JpaRepository<PostTag, Long> {
+
+}
