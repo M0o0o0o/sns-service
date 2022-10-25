56acdd06ff6ccbb0597f6d38546c91d0a5f151c3
Lee-moo <dlandif22@gmail.com>
2022-10-25 20:21:01 +0900

    Nothing change

--- src/main/java/mooyeol/snsservice/SnsServiceApplication.java
+++ src/main/java/mooyeol/snsservice/SnsServiceApplication.java
@@ -2,15 +2,12 @@ package mooyeol.snsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class SnsServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SnsServiceApplication.class, args);
 	}
-
 }
