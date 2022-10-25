c75273dc3dc0d33a3d4cc1e42bfa7d2b2370e910
Lee-moo <dlandif22@gmail.com>
2022-10-25 20:19:33 +0900

    Add new PostRepository and Custom Repository

---
+++ src/main/java/mooyeol/snsservice/repository/PostRepository.java
@@ -0,0 +1,24 @@
+package mooyeol.snsservice.repository;
+
+import mooyeol.snsservice.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    /**
+     * save
+     * delete
+     * findPost -> findById
+     */
+
+    /**
+     * @Todo findPostWithHashTags(Long id)
+     * @Todo findPosts(String order, String desc, String search, List < String > listAhshtaglks
+     */
+
+    @Query("select distinct p from Post p join fetch p.postTags where p.id = :id")
+    Optional<Post> findPostWithHashTags(@Param("id") Long id);
+}
