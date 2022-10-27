b4fa0bfc55462ac1c7f06621ed4b8d7671892f71
Lee-moo <dlandif22@gmail.com>
2022-10-27 11:57:32 +0900

    Add new PostRepository, PostRepositoryCustom, PostRepositoryImpl

--- src/main/java/mooyeol/snsservice/repository/PostRepository.java
+++ src/main/java/mooyeol/snsservice/repository/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom{
     /**
      * save
      * delete
