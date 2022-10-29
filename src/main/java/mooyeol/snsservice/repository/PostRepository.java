f10eebd080ec6eedd4a572318b494004f0c38905
Lee-moo <dlandif22@gmail.com>
2022-10-29 18:18:37 +0900

    Init git cache

--- src/main/java/mooyeol/snsservice/repository/PostRepository.java
+++ src/main/java/mooyeol/snsservice/repository/PostRepository.java
@@ -8,17 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom{
-    /**
-     * save
-     * delete
-     * findPost -> findById
-     */
-
-    /**
-     * @Todo findPostWithHashTags(Long id)
-     * @Todo findPosts(String order, String desc, String search, List < String > listAhshtaglks
-     */
-
     @Query("select distinct p from Post p join fetch p.postTags where p.id = :id")
     Optional<Post> findPostWithHashTags(@Param("id") Long id);
 }
