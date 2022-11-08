8754962a631de038985a51834c6ff82ccee6c1ca
Lee-moo <dlandif22@gmail.com>
2022-11-08 13:43:16 +0900

    Update SpringConfig

--- src/main/java/mooyeol/snsservice/config/SpringConfig.java
+++ src/main/java/mooyeol/snsservice/config/SpringConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class SpringConfig {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Bean
-    public JPAQueryFactory queryFactory(EntityManager em) {
+    public JPAQueryFactory queryFactory() {
         return new JPAQueryFactory(em);
     }
 }
