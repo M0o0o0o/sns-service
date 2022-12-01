2373b3144f5886260e2b4e5f982089ee2eb563f8
Lee-moo <dlandif22@gmail.com>
2022-12-01 18:35:20 +0900

    Feature ReplyController, ReplyService

---
+++ src/main/java/mooyeol/snsservice/controller/ReplyController.java
@@ -0,0 +1,7 @@
+package mooyeol.snsservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReplyController {
+}
---
+++ src/main/java/mooyeol/snsservice/service/ReplyService.java
@@ -0,0 +1,8 @@
+package mooyeol.snsservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Service
+public class ReplyService {
+}
